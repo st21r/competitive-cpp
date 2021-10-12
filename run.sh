73f67293be4d44574ef8ffaacc1030ce1086a6f5
sk4rdz <3st021c@gmail.com>
2021-10-12 10:09:27 +0900

    u

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 g++ solve.cpp -std=c++17 -D=LOCAL -Wall -Wextra -Wno-unknown-pragmas -g -fsanitize=undefined -D_GLIBCXX_DEBUG 
-./a.out < input.txt
-rm a.out
+
+if [ -e a.out ]; then
+    ./a.out < input.txt
+    rm -f a.out
+fi
